e6191140cffb7c4cd7fe80af728b1be801cbaab6
Priyams-Bajracharya <priyams.bajracharya@gmail.com>
2025-04-26 00:24:03 +0545

    Added a single function counter instead of individual functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,16 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'first-ng-app';
   count = 0;
-  handleIncrement(){
-    this.count++;
-  }
-  handleDecrement(){
-    this.count--;
-  }
-  resetCounter(){
-    this.count=0;
+  
+  handleCounter(val:string){
+    if(val == 'plus'){
+      this.count++;
+    }
+    else if (val=='minus'&& this.count > 0){
+      this.count--;
+    }
+    else if (val == 'reset'){
+      this.count=0;
+    }
   }
 }
